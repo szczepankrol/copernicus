a8768ed67ce127bee753008e32ad00c16c1e8330
Piotr Soluch <piotr@soluch.com>
2011-11-13 23:38:29 +0100

    [initial import]

---
+++ copernicus/footer.php
@@ -0,0 +1,5 @@
+		
+<?php wp_footer(); ?>
+		<script src="<?php bloginfo('template_directory'); ?>/static/js/script.js"></script>
+	</body>
+</html>
---
+++ copernicus/header.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+	<head>
+		<meta charset="<?php bloginfo('charset'); ?>" />
+		<title><?php
+		      if (function_exists('is_tag') && is_tag()) {
+		         single_tag_title("Tag Archive for &quot;"); echo '&quot; - '; }
+		      elseif (is_archive()) {
+		         wp_title(''); echo ' Archive - '; }
+		      elseif (is_search()) {
+		         echo 'Search for &quot;'.wp_specialchars($s).'&quot; - '; }
+		      elseif (!(is_404()) && (is_single()) || (is_page())) {
+		         wp_title(''); echo ' - '; }
+		      elseif (is_404()) {
+		         echo 'Not Found - '; }
+		      if (is_home()) {
+		         bloginfo('name'); echo ' - '; bloginfo('description'); }
+		      else {
+		          bloginfo('name'); }
+		      if ($paged>1) {
+		         echo ' - page '. $paged; } ?></title>
+		<meta name="title" content="<?php
+		      if (function_exists('is_tag') && is_tag()) {
+		         single_tag_title("Tag Archive for &quot;"); echo '&quot; - '; }
+		      elseif (is_archive()) {
+		         wp_title(''); echo ' Archive - '; }
+		      elseif (is_search()) {
+		         echo 'Search for &quot;'.wp_specialchars($s).'&quot; - '; }
+		      elseif (!(is_404()) && (is_single()) || (is_page())) {
+		         wp_title(''); echo ' - '; }
+		      elseif (is_404()) {
+		         echo 'Not Found - '; }
+		      if (is_home()) {
+		         bloginfo('name'); echo ' - '; bloginfo('description'); }
+		      else {
+		          bloginfo('name'); }
+		      if ($paged>1) {
+		         echo ' - page '. $paged; }
+		   ?>" />
+		<meta name="description" content="<?php bloginfo('description'); ?>" />
+		<link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/static/img/favicon.ico">
+		<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/static/css/style.css" media="all" />
+		<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/static/css/print.css" media="print" />
+		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+		<?php wp_head(); ?>
+	</head>
+	<body>
