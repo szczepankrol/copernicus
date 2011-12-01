682278c7da1cb7d16d42c9cb1d35ee2ce5a9a731
Piotr Soluch <piotr@soluch.com>
2011-12-01 01:10:21 +0100

    [new] nested js file, some small development

--- copernicus/lib/copernicus/class-admin-cp.php
+++ copernicus/lib/copernicus/class-admin-cp.php
@@ -13,13 +13,27 @@ class admin_cp {
 
 	function __construct() {
 
+
 		add_action('admin_init', array($this, 'load_js'));
+		add_action('admin_init', array($this, 'load_css'));
 		add_action('dbx_post_advanced', array($this, 'page_save_dialog'));
 	}
 
 	public function load_js() {
-		wp_register_script('cp_admin', CP_STATIC_DIR . '/js/cp-admin.js', 'jquery', CP_VERSION, 1);
+		wp_enqueue_script('jquery');
+		wp_enqueue_script('jquery-ui-core');
+		wp_enqueue_script('jquery-ui-sortable');
+		
+		wp_register_script('jquery-ui-nested', CP_STATIC_DIR . '/js/jquery.ui.nestedSortable.js', array('jquery','jquery-ui-core', 'jquery-ui-sortable'), CP_VERSION, 1);
+		wp_enqueue_script('jquery-ui-nested');
+		
+		wp_register_script('cp_admin', CP_STATIC_DIR . '/js/cp-admin.js', array('jquery','jquery-ui-core', 'jquery-ui-sortable'), CP_VERSION, 1);
 		wp_enqueue_script('cp_admin');
+	}
+	
+	public function load_css() {
+		wp_register_style('cp_admin', CP_STATIC_DIR . '/css/cp-admin.css', '', CP_VERSION, 'all');
+		wp_enqueue_style('cp_admin');
 	}
 
 	public function page_save_dialog() {
--- copernicus/static/js/cp-admin.js
+++ copernicus/static/js/cp-admin.js
@@ -1,5 +1,6 @@
 jQuery(document).ready(function($) {
 	page_order($);
+	drag_n_drop($);
 });
 
 function page_order($) {
@@ -10,4 +11,32 @@ function page_order($) {
 	
 	menu_order = $('#menu_order').val();
 	$('#menu_order').parent().parent().append('<input type="hidden" name="menu_order" value="'+menu_order+'" />');
+}
+
+function drag_n_drop($) {
+	$('#the-list tr th').prepend('<div class="sort-handler"></div>');
+	
+	tr_w = $('#the-list tr').width();
+	$('#the-list tr').css('width', tr_w);
+	
+	$('#the-list tr').each(
+		function(){
+			$(this).children('td').each(
+				function(){
+					tr_ww = $(this).width();
+					$(this).css('width', tr_ww);
+				}
+			);
+		}
+	);
+		
+	$('#the-list').nestedSortable({
+		handle : '.sort-handler',
+		axis:'y',
+		containment: 'tbody',
+		update: function(event, ui) {
+			$('#the-list tr').removeClass('alternate');
+			$('#the-list tr:even').addClass('alternate');
+		}
+	});
 }
