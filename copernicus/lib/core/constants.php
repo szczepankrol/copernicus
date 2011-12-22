1bdb7d2b3ee0be542e20da72ea024d47df96e4ec
Piotr Soluch <piotr@soluch.com>
2011-12-22 12:49:14 +0100

    [changed] moved constants around

--- copernicus/lib/core/constants.php
+++ copernicus/lib/core/constants.php
@@ -34,12 +34,12 @@ define('CP_PHPTHUMB', '');
 define('CP_BASE_PATH', ABSPATH); // wordpress base path
 define('CP_THEME_PATH', get_stylesheet_directory());
 define('CP_LIB_PATH', CP_COPERNICUS_PATH . '/lib');
-define('CP_CACHE_PATH', CP_BASE_PATH . '/wp-content/cache');
+define('CP_CACHE_PATH', CP_BASE_PATH . 'wp-content/cache');
 
 define('CP_BASE_URL', get_bloginfo('wpurl'));
-define('CP_THEME_URL', get_bloginfo('template_url'));
+define('CP_COPERNICUS_URL', get_template_directory_uri());
 
-define('CP_THEME_DIR', str_replace(CP_BASE_URL, '', CP_THEME_URL));
-define('CP_STATIC_DIR', CP_THEME_DIR . '/static');
+define('CP_COPERNICUS_DIR', str_replace(CP_BASE_URL, '', CP_COPERNICUS_URL));
+define('CP_COPERNICUS_STATIC_DIR', CP_COPERNICUS_DIR . '/static');
 
 ?>
