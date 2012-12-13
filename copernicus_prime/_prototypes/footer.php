4fdae73c8417194c9e8d6871e96a986a941f32fa
Piotr Soluch <piotr@soluch.com>
2012-12-13 17:46:43 +0100

    now only copernicus

--- copernicus_prime/_prototypes/footer.php
+++
@@ -1,8 +0,0 @@
-	<footer>
-		
-	</footer>
-	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
-	<script>window.jQuery || document.write('<script src="/_works/prototypes/static/js/jquery-1.8.0.min.js"><\/script>')</script>
-	<script defer src="/_works/prototypes/static/js/script.js"></script>
-</body>
-</html>
