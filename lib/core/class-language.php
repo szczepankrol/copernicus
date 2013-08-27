e9324552e25eb6496b8de40ef67ac1b368287053
Piotr Soluch <piotr@soluch.com>
2013-08-27 12:18:02 +0200

    [changed] few minor improvements in language class

--- lib/core/class-language.php
+++ lib/core/class-language.php
@@ -27,7 +27,7 @@ class CP_Language {
 	 * @author Piotr Soluch
 	 */
 	public function __construct() {
-		
+
 		// initialize the meta boxes
 		$this->_init();
 	}
@@ -41,35 +41,41 @@ class CP_Language {
 	 */
 	public function _init() {
 
-		if (isset($_GET['lang'])) {
-			$this->change_language($_GET['lang']);
+		if ( isset( $_GET['lang'] ) ) {
+			$this->change_language( $_GET['lang'] );
 		}
 
 		// get current language
 		$current_language = $this->get_current_language();
 
-		$this->define_current_language($current_language);
+		$this->define_current_language( $current_language );
 	}
 
-	public function get_languages($status = 1) {
+	private function define_current_language( $current_language ) {
+		$this->current_language = $current_language;
+		define( 'LANGUAGE', $current_language['code'] );
+		define( 'LANGUAGE_SUFFIX', $current_language['postmeta_suffix'] );
+	}
+
+	public function get_languages( $status = 1 ) {
 		$languages = CP::$config['language'];
-		
-		if ($status != 'all') {
-			
-			foreach ($languages as $key => $language) {
-				if ($language['status'] != $status) {
-					unset($languages[$key]);
+
+		if ( $status != 'all' ) {
+
+			foreach ( $languages as $key => $language ) {
+				if ( $language['status'] != $status ) {
+					unset( $languages[$key] );
 				}
 			}
 		}
 
 		return $languages;
 	}
-	
-	public function get_language($code = '') {
-		foreach (CP::$config['language'] as $language) {
-			if ($code) {
-				if ($code == $language['code']) {
+
+	public function get_language( $code = '' ) {
+		foreach ( CP::$config['language'] as $language ) {
+			if ( $code ) {
+				if ( $code == $language['code'] ) {
 					return $language;
 				}
 			}
@@ -79,57 +85,52 @@ class CP_Language {
 	}
 
 	public function get_current_language() {
-		if (isset($_SESSION['language'])) {
-			return $this->get_language($_SESSION['language']);
+		if ( isset( $_SESSION['language'] ) ) {
+			return $this->get_language( $_SESSION['language'] );
 		}
-		
-		if (isset ($_COOKIE['language'])) {
-			return $this->get_language($_COOKIE['language']);
+
+		if ( isset ( $_COOKIE['language'] ) ) {
+			return $this->get_language( $_COOKIE['language'] );
 		}
 
 		return $this->get_default_language();
-	} 
+	}
 
 	public function get_default_language() {
-		if ($this->language) {
-			foreach ($this->language as $language) {
-				if ($language['default']) {
-					return $language;
-				}
+		if ( $this->current_language )
+			return $this->current_language;
+
+		foreach ( CP::$config['language'] as $language ) {
+			if ( $language['default'] ) {
+				return $language;
 			}
 		}
 
 		return null;
 	}
 
-	private function set_current_language($current_language, $remember = true) {
-		//
+	/**
+	 * change language and redirecto to a previous page
+	 *
+	 * @param string  $language language code
+	 * @return none
+	 */
+	private function change_language( $language ) {
+		$this->set_current_language( $language );
+
+		wp_redirect( $_SERVER['HTTP_REFERER'] );
+		exit;
+	}
+
+	private function set_current_language( $current_language, $remember = true ) {
+		// set session for current language
 		$_SESSION['language'] = $current_language['code'];
-	
-		if ($remember) {
+
+		if ( $remember ) {
 			$expire = 60 * 60 * 24 * 31; // a month
 
 			// set cookie for current language
-			setcookie('language', $current_language['code'], time() + $expire);
+			setcookie( 'language', $current_language['code'], time() + $expire );
 		}
-		
-	}
-
-	private function define_current_language($current_language) {
-		define('LANGUAGE', $current_language['code']);
-		define('LANGUAGE_SUFFIX', $current_language['postmeta_suffix']);
-	}
-	
-	/**
-	 * change language and redirecto to a previous page
-	 * 
-	 * @param  string $language language code
-	 * @return none
-	 */
-	private function change_language($language) {
-		$_SESSION['language'] = $language;
-		
-		wp_redirect($_SERVER['HTTP_REFERER']);
-		exit;
 	}
 }
